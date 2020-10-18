0844257e934bd211a378a1b8bb8134c51d170193
pedroluz-dev <pedro.luz@aluno.faculdadeimpacta.com.br>
2020-10-18 13:25:33 -0300

    ac3 changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python3 t8.py
--- t8.py
+++ t8.py
@@ -1,18 +1,42 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask import jsonify
+from flask import request
 from math import sqrt
 
 app = Flask(__name__)
 
 @app.route('/')
-def nao_entre_em_panico():
 
 
-    primos = "Tudo vai dar certo caros alunos!"
+def ehprimo(numero):
+    primo = True
+    
+    if (numero == 1 ):
+        primo = False
+    else:
+        primo = True
 
+    for i in range (2, numero):
+        if (numero % i == 0):
+            primo = False
+    return primo
+
+def cade_os_primos ():
+    numero = 1
+    primos = []
+    while len(primos) <= 100:
+        if (ehprimo(numero)):
+            primos.append(numero)
+        numero+= 1
 
     return primos
+
+cade_os_primos()
+            
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+    
