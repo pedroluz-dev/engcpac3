37e504ff2408e4eabe93808128dadd91360752bc
pedroluz-dev <pedro.luz@aluno.faculdadeimpacta.com.br>
2020-10-18 14:01:17 -0300

    try1

--- t8.py
+++ t8.py
@@ -8,28 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 
-
-
-def primo(num):
-   if num != 1:
-         t = True
-         for i in range(2,num):
-             if (num % i == 0):
-                 t = False
-                 break
-         return t
-         
-e = []
-lim = 100
- 
-for num in range(2, lim + 1):
-
-    test = primo(num)
-
-    if (test):
-        e.append(num)
-        return e
-
+def primos():
+    primos = "Somos primos"
+    return primos
+    
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
